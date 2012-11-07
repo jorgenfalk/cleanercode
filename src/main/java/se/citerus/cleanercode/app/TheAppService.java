015daad8d94cf86e50c971ae47eefd0b70add58d
Jörgen Falk <jorgen.p.falk@gmail.com>
2012-11-07 13:21:14 +0100

    Removed static instance SenderRepository in App Extracted an interface from SenderRepository for compliance with the interface segregation principle Refactor AppTest to use Mockito mocks Added new Guice @provider for SenderRepository

--- src/main/java/se/citerus/cleanercode/app/TheAppService.java
+++ src/main/java/se/citerus/cleanercode/app/TheAppService.java
@@ -1,6 +1,7 @@
 package se.citerus.cleanercode.app;
 
 import com.google.inject.Inject;
+import org.apache.commons.lang.Validate;
 import se.citerus.cleanercode.model.Campaign;
 import se.citerus.cleanercode.model.Customer;
 import se.citerus.cleanercode.model.CustomerType;
@@ -29,30 +30,37 @@ public class TheAppService implements AppService{
     public void onTimerEvent() {
         final List<Customer> customers = customerRepository.getNewCustomers();
         final Campaign campaign = new HalfOfCampaign();
-        final Set<Long> ids = new HashSet<Long>();
 
-        if (customers == null) {
-            throw new IllegalArgumentException("No new customers found");
-        } else {
-            List<Customer> temp = new ArrayList<Customer>();
-            for (Customer c : customers){
-                Long id = c.id();
-                ids.add(id);
+        Validate.notNull(customers, "Error initializing the CustomerRepository");
+
+        for (Customer c : getUniqueCustomers(customers)) {
+            if (customersOfGreatValue(c)){
+                senderRepository.findFastestSenderService().send(campaign.message(), c);
             }
-            for (Long id : ids) {
-                for (Customer c : customers) {
-                    if (c.id().equals(id)){
-                        temp.add(c);
-                    }
-                }
-            }
-            for (Customer c : temp) {
-                if ((c.type() == CustomerType.GOLD
-                    || c.type() == CustomerType.VIP)
-                    && c.balance().over(1000.00)){
-                    senderRepository.findFastestSenderService().send(campaign.message(), c);
+        }
+    }
+
+
+    private boolean customersOfGreatValue(Customer c) {
+        return (c.type() == CustomerType.GOLD
+                || c.type() == CustomerType.VIP)
+                && c.balance().over(1000.00);
+    }
+
+    private List<Customer> getUniqueCustomers(List<Customer> customers) {
+        final Set<Long> ids = new HashSet<Long>();
+        List<Customer> temp = new ArrayList<Customer>();
+        for (Customer c : customers){
+            Long id = c.id();
+            ids.add(id);
+        }
+        for (Long id : ids) {
+            for (Customer c : customers) {
+                if (c.id().equals(id)){
+                    temp.add(c);
                 }
             }
         }
+        return temp;
     }
 }
