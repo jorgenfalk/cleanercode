fa868670270135e0cf793d267bfe870d706b4b03
Jörgen Falk <jorgen.p.falk@gmail.com>
2012-11-12 11:30:31 +0100

    Minor stuff

--- src/test/java/se/citerus/cleanercode/app/AppTest.java
+++ src/test/java/se/citerus/cleanercode/app/AppTest.java
@@ -14,16 +14,16 @@ import java.util.Arrays;
  */
 public class AppTest {
     @Test
-      public void onTimerEvent(){
+    public void onTimerEvent(){
         AppService service = new TheAppService(createInMemCustomerRepository());
         service.onTimerEvent();
-      }
+    }
 
-      private CustomerRepository createInMemCustomerRepository(){
+    private CustomerRepository createInMemCustomerRepository(){
         return new InMemCustomerRepository(Arrays.asList(
                 new Customer(1L, "Mr X", new Money(987.65), CustomerType.BASIC),
                 new Customer(2L, "Ms V", new Money(1234.56), CustomerType.BASIC),
                 new Customer(3L, "Ms Y", new Money(2345.67), CustomerType.GOLD),
                 new Customer(4L, "Mr Z", new Money(3456.78), CustomerType.VIP)));
-      }
+    }
 }
